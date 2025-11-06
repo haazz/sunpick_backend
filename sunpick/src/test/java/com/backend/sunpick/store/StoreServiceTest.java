c10c06e59479af8e99c8d2a91aa1e01ebcb47443
haazz <gkwldnjs121@gmail.com>
2025-11-06 14:53:59 +0900

    feat: StoreServiceTest class modifyStore() 테스트

--- sunpick/src/test/java/com/backend/sunpick/store/StoreServiceTest.java
+++ sunpick/src/test/java/com/backend/sunpick/store/StoreServiceTest.java
@@ -3,6 +3,7 @@ package com.backend.sunpick.store;
 import com.backend.sunpick.domain.member.entity.Member;
 import com.backend.sunpick.domain.member.repository.MemberRepository;
 import com.backend.sunpick.domain.store.dto.request.StoreCreateRequest;
+import com.backend.sunpick.domain.store.dto.request.StoreModifyRequest;
 import com.backend.sunpick.domain.store.entity.Store;
 import com.backend.sunpick.domain.store.repository.StoreRepository;
 import com.backend.sunpick.domain.store.service.StoreService;
@@ -14,14 +15,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -50,7 +53,7 @@ public class StoreServiceTest {
         storeService.createStore(request);
 
         ArgumentCaptor<Store> captor = ArgumentCaptor.forClass(Store.class);
-        verify(storeRepository, Mockito.times(1)).save(captor.capture());
+        verify(storeRepository, times(1)).save(captor.capture());
         Store saved = captor.getValue();
 
         assertThat(saved.getName()).isEqualTo("storeName");
@@ -68,31 +71,73 @@ public class StoreServiceTest {
         NoSuchElementException exception = assertThrows(NoSuchElementException.class,
             () -> storeService.createStore(request));
         assertEquals("회원 ID: " + NON_EXISTENT_ID + "가 존재하지 않습니다.", exception.getMessage());
-        verify(storeRepository, Mockito.never()).save(any(Store.class));
+        verify(storeRepository, never()).save(any(Store.class));
     }
 
     @Test
     @DisplayName("modifyStore() - 성공")
     void modifyStore_success() {
+        Member oldOwner = mock(Member.class);
+        Store store = Store.builder()
+                .name("name")
+                .description("description")
+                .ownerName("ownerName")
+                .member(oldOwner)
+                .build();
+        when(storeRepository.findById(1)).thenReturn(Optional.of(store));
 
+        Member newOwner = mock(Member.class);
+        when(newOwner.getName()).thenReturn("newOwnerName");
+        when(memberRepository.findById(2)).thenReturn(Optional.of(newOwner));
+
+        StoreModifyRequest request = new StoreModifyRequest(2, "newName", "newDescription");
+
+        storeService.modifyStore(1, request);
+
+        assertEquals("newName", store.getName());
+        assertEquals("newDescription", store.getDescription());
+        assertEquals(newOwner, store.getMember());
+        assertEquals("newOwnerName", store.getOwnerName());
     }
 
     @Test
     @DisplayName("modifyStore() - 실패 storeId가 존재하지 않는 경우")
     void modifyStore_fail_storeNotFound() {
+        StoreModifyRequest request = new StoreModifyRequest(1, "newName", "newDescription");
+        when(storeRepository.findById(any(Integer.class))).thenReturn(Optional.empty());
 
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class,
+            () -> storeService.modifyStore(NON_EXISTENT_ID, request));
+
+        assertEquals("상점 ID: " + NON_EXISTENT_ID + "가 존재하지 않습니다.", exception.getMessage());
     }
 
     @Test
     @DisplayName("modifyStore() - 실패 storeId가 삭제된 경우")
     void modifyStore_fail_deletedStore() {
+        Store store = mock(Store.class);
+        when(store.isDeleted()).thenReturn(true);
+        when(storeRepository.findById(1)).thenReturn(Optional.of(store));
+        StoreModifyRequest request = new StoreModifyRequest(1, "newName", "newDescription");
 
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class,
+            () -> storeService.modifyStore(1, request));
+        assertEquals("상점 ID: " + 1 + "는 삭제되었습니다.", exception.getMessage());
     }
 
     @Test
     @DisplayName("modifyStore() - 실패 memberId가 존재하지 않는 경우")
     void modifyStore_fail_memberNotFound() {
+        Store store = mock(Store.class);
+        when(store.isDeleted()).thenReturn(false);
+        when(storeRepository.findById(1)).thenReturn(Optional.of(store));
+        when(memberRepository.findById(NON_EXISTENT_ID)).thenReturn(Optional.empty());
 
+        StoreModifyRequest request = new StoreModifyRequest(NON_EXISTENT_ID, "newName", "newDescription");
+
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class,
+            () -> storeService.modifyStore(1, request));
+        assertEquals("회원 ID: " + NON_EXISTENT_ID + "가 존재하지 않습니다.", exception.getMessage());
     }
 
     @Test
